77add0550f1add1493875a41c6e843f7749838f2
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-03-29 00:48:10 -0600

    add js function

--- src/js/catalog-show-more.js
+++ src/js/catalog-show-more.js
@@ -1,4 +1,14 @@
-// to open and close modal window
+// to show more 5-th item
+document.querySelectorAll('.btn-catalog').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document
+      .querySelector('.catalog-item:nth-child(5)')
+      .classList.toggle('show-more');
+  });
+});
+
+// to show more 6-th item
+
 document.querySelectorAll('.btn-catalog').forEach(btn => {
   btn.addEventListener('click', () => {
     document
@@ -6,3 +16,5 @@ document.querySelectorAll('.btn-catalog').forEach(btn => {
       .classList.toggle('show-more');
   });
 });
+
+//
